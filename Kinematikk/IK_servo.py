78d61d01daf0b94f0754a22cc5ece50d55033caa
Sjoulk <shd@live.no>
2025-03-03 11:31:05 +0100

    transformAngleAdaptArm ferdig

--- Kinematikk/IK_servo.py
+++ Kinematikk/IK_servo.py
@@ -48,7 +48,7 @@ class ArmIK:
     def transformAngelAdaptArm(self, theta3, theta4, theta5, theta6):
         # Konverterer vinkler fra IK_geo til korresponderende pulslengder
         # Vinkel [deg] * 11.11 [us/deg] + 2500[us] + (500/2)[us]
-        
+
         servo3 = int(round(theta3 * self.servo3Param + (self.servo3Range[1] + self.servo3Range[0])/2))
         if servo3 > self.servo3Range[1] or servo3 < self.servo3Range[0]:
             # Sjekker om vinkelutslag er innenfor gyldig område
@@ -60,4 +60,23 @@ class ArmIK:
             # Sjekker om vinkelutslag er innenfor gyldig område
             logger.info('servo4(%s)Utenfor gyldig område(%s, %s)', servo4, self.servo4Range[0], self.servo4Range[1])
             return False
+        
+        servo5 = int(round((self.servo5Range[1] + self.servo5Range[0])/2 + (90.0 - theta5) * self.servo5Param)) 
+        if servo5 > ((self.servo5Range[1] + self.servo5Range[0])/2 + 90*self.servo5Param) or servo5 < ((self.servo5Range[1] + self.servo5Range[0])/2 - 90*self.servo5Param):
+            # Sjekker om vinkelutslag er innenfor gyldig område
+            logger.info('servo5(%s)Utenfor gyldig område(%s, %s)', servo5, self.servo5Range[0], self.servo5Range[1])
+            return False
+        
+        # Sjekker hvilken kvadrant servo6 skal rotere til
+        if theta6 < -(self.servo6Range[3] - self.servo6Range[2])/2:
+            # Hvis theta6 < -180 [deg] -> servo6 = (180-0)/2 + 90 + (180 + theta6)
+            servo6 = int(round(((self.servo6Range[3] - self.servo6Range[2])/2 + (90 + (180 + theta6))) * self.servo6Param))
+        else:
+            # Hvis ikke: servo6 = ((180-0)/2 -(90-theta6))* 11.11us + 500us 
+            servo6 = int(round(((self.servo6Range[3] - self.servo6Range[2])/2 - (90 - theta6)) * self.servo6Param)) + self.servo6Range[0]
+        if servo6 > self.servo6Range[1] or servo6 < self.servo6Range[0]:
+            # Sjekker om vinkelutslag er innenfor gyldig område
+            logger.info('servo6(%s)Utenfor gyldig område(%s, %s)', servo6, self.servo6Range[0], self.servo6Range[1])
+            return False
+        return {"servo3": servo3, "servo4": servo4, "servo5": servo5, "servo6": servo6}
         
