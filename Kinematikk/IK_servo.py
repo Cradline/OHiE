dbb29b51a6b9e59850d737b340a3c0e90b37dcf3
Sjoulk <shd@live.no>
2025-03-03 11:17:38 +0100

    def transformAngleAdaptArm

--- Kinematikk/IK_servo.py
+++ Kinematikk/IK_servo.py
@@ -24,9 +24,9 @@ ik.setLinkLength(L1=l1+1.3, L4=l4)
 # Klasse for servokontroll
 class ArmIK:
     # Pulsbredde intervall [us], vinkelintervall [deg]
-    servo3Range = (500, 2500.0, 0, 180.0)       # Servo ID3: 
-    servo4Range = (500, 2500.0, 0, 180.0)       # Servo ID4: 
-    servo5Range = (500, 2500.0, 0, 180.0)       # Servo ID5: 
+    servo3Range = (500, 2500.0, 0, 180.0)       # Servo ID3: ledd C
+    servo4Range = (500, 2500.0, 0, 180.0)       # Servo ID4: ledd B
+    servo5Range = (500, 2500.0, 0, 180.0)       # Servo ID5: ledd A
     servo6Range = (500, 2500.0, 0, 180.0)       # Servo ID6: Base-ledd
 
     def __init__(self):
@@ -44,3 +44,20 @@ class ArmIK:
         self.servo4Param = ((self.servo4Range[1] - self.servo4Range[0]) / (self.servo4Range[3] - self.servo4Range[2]))
         self.servo5Param = ((self.servo5Range[1] - self.servo5Range[0]) / (self.servo5Range[3] - self.servo5Range[2]))
         self.servo6Param = ((self.servo6Range[1] - self.servo6Range[0]) / (self.servo6Range[3] - self.servo6Range[2]))
+
+    def transformAngelAdaptArm(self, theta3, theta4, theta5, theta6):
+        # Konverterer vinkler fra IK_geo til korresponderende pulslengder
+        # Vinkel [deg] * 11.11 [us/deg] + 2500[us] + (500/2)[us]
+        
+        servo3 = int(round(theta3 * self.servo3Param + (self.servo3Range[1] + self.servo3Range[0])/2))
+        if servo3 > self.servo3Range[1] or servo3 < self.servo3Range[0]:
+            # Sjekker om vinkelutslag er innenfor gyldig område
+            logger.info('servo3(%s)Utenfor gyldig område(%s, %s)', servo3, self.servo3Range[0], self.servo3Range[1])
+            return False
+        
+        servo4 = int(round(theta4 * self.servo4Param + (self.servo4Range[1] + self.servo4Range[0])/2))
+        if servo4 > self.servo4Range[1] or servo4 < self.servo4Range[0]:
+            # Sjekker om vinkelutslag er innenfor gyldig område
+            logger.info('servo4(%s)Utenfor gyldig område(%s, %s)', servo4, self.servo4Range[0], self.servo4Range[1])
+            return False
+        
